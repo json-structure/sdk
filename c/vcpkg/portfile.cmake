2b8537c0f0978504e9623fa43dbe25d162af8c4c
Clemens Vasters <clemensv@microsoft.com>
2025-12-04 16:33:14 +0100

    chore(c): update vcpkg port to v0.4.0 and clean up portfile

--- c/vcpkg/portfile.cmake
+++ c/vcpkg/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO json-structure/sdk
     REF "v${VERSION}"
-    SHA512 0  # This will need to be updated with the actual SHA512 hash
+    SHA512 a572f5e6dfede6f4f7932f955c114f9f316ba35dafe2570e0a6293a1325b25000b794715d3a11f49c933afc31b64f6a8ce5366f5e1399f0d3c24e4309f62d0a6
     HEAD_REF master
 )
 
